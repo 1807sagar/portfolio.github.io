fea5f20cc87f6dc2c245ccbf7358df7add823d3b
1807sagar <1807sagarpratapsinghrajput@gmail.com>
2022-11-18 17:45:42 +0530

    Two FIles Added

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -22,7 +22,7 @@ export const Hero = () => {
             </h1>
             <p data-aos='fade-left'>{val.desc}</p>
             <button className='primaryBtn' data-aos='fade-up-right'>
-              <a href="https://www.linkedin.com/dms/D5606AQGUmfsLLNZ2VQ/messaging-attachmentFile/0/1667422133411?m=AQIOJR1VKwEo3AAAAYQ8fUX6A0hXsF0kiWpP9wgz2BrISt2gKGm_mC2etA&ne=1&v=beta&t=7lkM0IjzfIejG7QpwzApOM-FC0PwPXapoVdLBdoufqM&lipi=urn%3Ali%3Apage%3Ad_flagship3_messaging%3B7NbHEtS5RuG2yAAF49RBfA%3D%3D">Download CV</a>
+              <a href="https://drive.google.com/file/d/1-G0lYwml_Jo8VAYWoqXnqFhgp3sQFIjU/view?usp=share_link">Download CV</a>
             </button>
           </div>
         ))}
